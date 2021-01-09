132628c13d757031a5c74cd76751c9ed5d184f70
bobbyyy57 <bobbylerias@yahoo.com>
2021-01-09 13:31:33 -0800

    Added googletest and .gitmodules files; modified CMakeLists.txt, main2.cpp, test.cpp, and c-echo.h files to remove endline

--- src/main1.cpp
+++ src/main1.cpp
@@ -6,8 +6,8 @@ using namespace std;
 int main()
 {
     Rectangle rect1, rect2;
-    rect.set_width(3);
-    rect.set_height(4);
+    rect1.set_width(3);
+    rect1.set_height(4);
 
     rect2.set_width(4);
     rect2.set_height(2);
